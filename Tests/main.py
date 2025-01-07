9c50a6cf269fc924e6cca389be6d3fa50481ab80
ManasKohli <manaskohli6@gmail.com>
2025-01-07 18:55:09 -0500

    updated test

--- Tests/main.py
+++ Tests/main.py
@@ -1,32 +1,3 @@
-import os
-
-
-todo = []
-count = 0
-
-def display_list():
-    print(f'list: {todo}')
-    
-
-
-print('Welcome to the to-do list')
-print('When all tasks are added type \'done\'')
-
-
-while True:
-    try:
-        
-        display_list()
-        
-        task = input('Add a task: ')
-        count += 1
-        todo.append(f'{count}:{task}')
-       
-    
-
-        if task.lower() == 'done':
-            break
-            display_list(count, task)
-    
-    except ValueError:
-        print('Error occured, please try again')
+st = {'eggz', 'shrew','ball'}
+st.insert('dog')
+print(st)

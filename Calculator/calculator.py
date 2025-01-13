8202727b4a3171c0a3302f778b81264d4cfcc59b
ManasKohli <manaskohli6@gmail.com>
2025-01-13 18:47:49 -0500

    updated calculator

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,48 +1,73 @@
-spaces = {
-    '0' : 0,
-    '1' : 1,
-    '2': 2,
-    '3' : 3,
-    '4' : 4,
-    '5' : 5,
-    '6' : 6,
-    '7' : 7,
-    '8' : 8,
-    '9' : 9,
-    '10' : 10,
-    '/' : '/',
-    'x' : 'x',
-    '-' : '-',
-    '+' : '+',
-    '=' : '='
-}
+cal = [
+    ['7', '8', '9', '/'],
+    ['4', '5', '6', 'x'],
+    ['1', '2', '3', '-'],
+    [' ', ' ', '0', '+']
+]
 
-count = 0
-
-def cal_row(a, b, c, d):
-    print( f'|{spaces[a]}||{spaces[b]}||{spaces[c]}||{spaces[d]}| \n')
+def board(cal):
+    for i in cal:
+       print(' '.join(map(str, i)))
 
 
-def display():
-    cal_row('7', '8', '9' , '/')
-    cal_row('4', '5', '6', '-')
-    cal_row('1','2','3','+')
-    print( f'   |{spaces['0']}|   |{spaces['=']}|')
-    
+def results(num1, num2, operator):
+    operations = {
+        '/': lambda x, y: round(x / y, 2) if y != 0 else "Undefined (division by zero)",
+        '+': lambda x, y: round(x + y, 2),
+        '-': lambda x, y: round(x - y, 2),
+        'x': lambda x, y: round(x * y, 2),
+        '^': lambda x, y: round(x ** y, 2)
+    }
+    result = operations[operator](num1, num2)
+    print(f'Result is: {result}')
+
+
+def check(x):
+    try:
+        float(x)
+        return True
+    except ValueError:
+        return False
+
 
 while True:
-    display()
-    while True:
-        count += 1
-        choice = float(input(f'Input number{count} to be calculated or type "=" to calculate: '))
-        operator = input ('operator (+,-,x,/): ')
+    board(cal)
 
-        if choice == '=':
-            break
+    num1 = input('First number or type "quit": ')
 
+    if num1.lower() == 'quit':
+        break
+
+    if not check(num1):
+        print('Only numbers are allowed.')
+        continue
+
+    num1 = float(num1)
+
+    try:
+        operator = input('Operator (/, +, -, x, ^): ')
+        if operator not in ('-', '+', '^', '/', 'x'):
+            print('Invalid operator.')
+            operator = input('Operator (/, +, -, x, ^): ')
+            
+    except ValueError:
+        print('Value error')
+        continue
+
+    num2 = input('Second number or type "quit": ')
+
+    if num2.lower() == 'quit':
+        break
+
+    if not check(num2):
+        print('Only numbers are allowed.')
+        num2 = input('Second number or type "quit": ')
+
+    num2 = float(num2)
+
+    # Call the `results` function with the operator
+    results(num1, num2, operator)
 
   
-
-
-
-
+    
+ 
